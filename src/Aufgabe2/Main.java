438e5a149e9cee085471a5d4d7492066b1fe04b6
Todiras Bogdan <bogdan.todiras@gmail.com>
2022-01-10 18:38:29 +0200

    Fourth commit

--- src/Aufgabe2/Main.java
+++ src/Aufgabe2/Main.java
@@ -1,8 +1,19 @@
 package Aufgabe2;
 
+import controller.Controller;
+import controller.ControllerInterface;
+import model.Produkt;
+import view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        List<Produkt> produktList = new ArrayList<>();
+        Controller controller = new Controller(produktList);
+        View view = new View(controller);
+        view.run();
     }
 }
